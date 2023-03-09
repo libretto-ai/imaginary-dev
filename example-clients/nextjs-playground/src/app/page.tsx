0ae8edfff5f7280c6cb0956389a5636e7bd87887
Alec Flett <alecf@flett.org>
2023-03-09 13:27:05 -0800

    try using the new metadata api

--- example-clients/nextjs-playground/src/app/page.tsx
+++ example-clients/nextjs-playground/src/app/page.tsx
@@ -1,6 +1,26 @@
 import { Playground } from "@/components/Playground";
 import "@uiw/react-textarea-code-editor/dist.css";
+import { Metadata } from "next";
+import logo from "./images/playground.png";
 
+export const metadata: Metadata = {
+  title: "Imaginary Programming Playground",
+  description:
+    "An online playground to try out writing and running new imaginary functions.",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+  },
+  icons: {
+    icon: "/favicon-32x32.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@imaginary_dev",
+    creator: "@imaginary_dev",
+    images: [logo.src],
+  },
+};
 export default function Home() {
   return (
     <main>
