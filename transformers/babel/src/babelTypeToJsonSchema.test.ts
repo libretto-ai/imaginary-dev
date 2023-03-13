c3a09fb176ac6c75f464f2a4475785b474c70a24
Alec Flett <alecf@flett.org>
2023-03-13 18:55:59 -0400

    test for babelTypetoJsonSchema

---
+++ transformers/babel/src/babelTypeToJsonSchema.test.ts
@@ -0,0 +1,47 @@
+import * as t from "@babel/types";
+import { JSONSchema7 } from "json-schema";
+import { babelTypeToJsonSchema } from "./babelTypeToJsonSchema";
+describe("babelTypeToJsonSchema", () => {
+  it.each<{ type: t.TSType; schema: JSONSchema7 }>([
+    {
+      type: t.tsBooleanKeyword(),
+      schema: {
+        type: "boolean",
+      },
+    },
+    {
+      type: t.tsNumberKeyword(),
+      schema: {
+        type: "number",
+      },
+    },
+    {
+      type: t.tsStringKeyword(),
+      schema: {
+        type: "string",
+      },
+    },
+    {
+      type: t.tsNullKeyword(),
+      schema: {
+        type: "null",
+      },
+    },
+    {
+      type: t.tsAnyKeyword(),
+      schema: {
+        type: "object",
+      },
+    },
+    {
+      type: t.tsArrayType(t.tsStringKeyword()),
+      schema: {
+        type: "array",
+        items: { type: "string" },
+      },
+    },
+  ])("should convert %p", ({ type, schema }) => {
+    const result = babelTypeToJsonSchema(type);
+    expect(result).toEqual(schema);
+  });
+});
