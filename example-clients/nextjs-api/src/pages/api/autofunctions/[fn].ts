6acac7f4c5c5b1c9de6faf1f5c770d6b1a075672
Alec Flett <alecf@flett.org>
2023-03-20 12:11:39 -0700

    demo of "auto" wrapping

---
+++ example-clients/nextjs-api/src/pages/api/autofunctions/[fn].ts
@@ -0,0 +1,3 @@
+import { imaginaryFunctionMap } from "@/emojify";
+import { wrapImaginaryFunctions } from "@imaginary-dev/nextjs-util";
+export default wrapImaginaryFunctions(imaginaryFunctionMap, __filename);
