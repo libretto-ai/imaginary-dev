7d34af0e0958db224492bdd0554263c723bb64f6
Sasha Aickin <sasha@aickin.com>
2023-03-21 15:43:17 -0400

    Interim checkin of better parsing of tsdoc comments.

--- util/src/ts-doc.test.ts
+++ util/src/ts-doc.test.ts
@@ -2,6 +2,7 @@ import {
   extractServiceParameters,
   isImaginaryCommentBlock,
   makeTSDocParser,
+  parseImaginaryComment,
 } from "./ts-doc";
 
 describe("extractServiceParameters", () => {
@@ -56,5 +57,93 @@ describe("isImaginaryCommentBlock", () => {
     const parser = makeTSDocParser();
     const parsed = parser.parseString(comment);
     expect(isImaginaryCommentBlock(parsed)).toEqual(isImaginary);
+  });
+});
+
+describe("parseImaginaryComment finds @imaginary", () => {
+  it.each([
+    { comment: "", isImaginary: false },
+    { comment: `/** @imaginary */`, isImaginary: true },
+    {
+      comment: `/** 
+    @imaginary
+    @badtaghere
+     */`,
+      isImaginary: true,
+    },
+  ])("should get imaginary status from %p", ({ comment, isImaginary }) => {
+    expect(parseImaginaryComment(comment) !== null).toEqual(isImaginary);
+  });
+});
+
+describe("parseImaginaryComment can get service parameters", () => {
+  it.each([
+    {
+      comment: `/** @imaginary */`,
+      params: {},
+    },
+    {
+      comment: `/** 
+      @imaginary
+      @openai \`{"temperature": 4}\`
+      */`,
+      params: {
+        openai: {
+          temperature: 4,
+        },
+      },
+    },
+    {
+      comment: `/** 
+      @imaginary
+      @openai \`{"max_tokens": 2}\`
+      */`,
+      params: {
+        openai: {
+          max_tokens: 2,
+        },
+      },
+    },
+  ])("Should extract parameters %p", ({ comment, params }) => {
+    expect(parseImaginaryComment(comment)?.getServiceParameters()).toEqual(
+      params
+    );
+  });
+});
+
+describe("parseImaginaryComment can remove service parameters", () => {
+  it.each([
+    {
+      comment: `/** 
+      @openai \`{"temperature": 0.7, "model": "gpt-4"}\`
+      * @imaginary
+      */`,
+      remove: ["openai", "temperature"],
+      result: `/**
+ * @openai
+ *
+ * \`{"model":"gpt-4"}\`
+ *
+ * @imaginary
+ */`,
+    },
+    {
+      comment: `/** 
+      @openai \`{"temperature": 0.7}\`
+      * @imaginary
+      */`,
+      remove: ["openai", "temperature"],
+      result: `/**
+ * @imaginary
+ */`,
+    },
+  ])("Should extract parameters %p", ({ comment, remove, result }) => {
+    const imaginaryComment = parseImaginaryComment(comment);
+
+    expect(
+      imaginaryComment
+        ?.removeSingleServiceParameter(remove[0], remove[1])
+        .getText()
+    ).toEqual(result);
   });
 });
--- util/src/ts-doc.ts
+++ util/src/ts-doc.ts
@@ -16,6 +16,112 @@ import {
 } from "@microsoft/tsdoc";
 import { AI_SERVICES, ServiceParameters } from "./function-definition";
 
+export function parseImaginaryComment(
+  comment: string
+): ImaginaryComment | null {
+  const parser: TSDocParser = makeTSDocParser();
+  const parsedComment = parser.parseString(comment);
+
+  if (!isImaginaryCommentBlock(parsedComment)) {
+    return null;
+  }
+
+  return new ImaginaryComment(parsedComment, comment);
+}
+
+class ImaginaryComment {
+  private parsedComment: ParserContext;
+  private rawComment: string;
+
+  constructor(parsedComment: ParserContext, rawComment: string) {
+    this.parsedComment = parsedComment;
+    this.rawComment = rawComment;
+  }
+
+  getServiceParameters(): ServiceParameters {
+    return extractServiceParametersFromParsedComment(this.parsedComment);
+  }
+
+  setSingleServiceParameter(
+    service: string,
+    parameter: string,
+    value: any
+  ): ImaginaryComment {
+    const blockTagInfo = getBlockTagInfo(this.parsedComment, `@${service}`);
+    if (!blockTagInfo) {
+      return this;
+    }
+    const { text, start, end } = blockTagInfo;
+    let settings = {};
+    try {
+      // TODO: use something other than JSON to parse this
+      settings = JSON.parse(text);
+    } catch (ex) {
+      console.warn("Failed to parse ", service, "tag: ", ex);
+    }
+    settings[parameter] = value;
+
+    const result = parseImaginaryComment(
+      this.rawComment.slice(0, start) +
+        `${service} \`${JSON.stringify(settings)}\`` +
+        this.rawComment.slice(end + 1)
+    );
+    if (result === null) {
+      throw new Error(
+        "Parsing error in ImaginaryComment.setSingleServiceParameter"
+      );
+    }
+    return result;
+  }
+
+  removeSingleServiceParameter(
+    service: string,
+    parameter: string
+  ): ImaginaryComment {
+    const mutableParsedComment = makeTSDocParser().parseString(this.rawComment);
+    const blockTagInfo = getBlockTagInfo(mutableParsedComment, `@${service}`);
+    if (!blockTagInfo) {
+      return this;
+    }
+    const { text, start, end } = blockTagInfo;
+    let settings = {};
+    try {
+      // TODO: use something other than JSON to parse this
+      settings = JSON.parse(text);
+    } catch (ex) {
+      console.warn("Failed to parse ", service, "tag: ", ex);
+    }
+    delete settings[parameter];
+
+    if (Object.keys(settings).length === 0) {
+    }
+
+    const block = getBlockTag(mutableParsedComment, `@${service}`);
+    if (!block) {
+      return this;
+    }
+    block.content.clearNodes();
+    const paragraph = new DocParagraph({
+      configuration: mutableParsedComment.configuration,
+    });
+    const codeSpan = new DocCodeSpan({
+      code: JSON.stringify(settings),
+      configuration: mutableParsedComment.configuration,
+    });
+    paragraph.appendNode(codeSpan);
+    block.content.appendNode(paragraph);
+
+    return new ImaginaryComment(
+      mutableParsedComment,
+      mutableParsedComment.docComment.emitAsTsdoc()
+    );
+  }
+
+  getText(): string {
+    return this.rawComment.trim();
+  }
+}
+
 export function makeTSDocParser() {
   const tsdocParser: TSDocParser = new TSDocParser();
   tsdocParser.configuration.addTagDefinition(
@@ -41,16 +147,24 @@ export function extractServiceParameters(
   parser: TSDocParser,
   funcComment: string
 ): ServiceParameters {
-  const parsedComment = parser.parseString(funcComment);
+  return extractServiceParametersFromParsedComment(
+    parser.parseString(funcComment)
+  );
+}
+
+function extractServiceParametersFromParsedComment(
+  parsedComment: ParserContext
+): ServiceParameters {
   const result: ServiceParameters = {};
   AI_SERVICES.forEach((serviceKey) => {
-    const tagText = getBlockTagContent(parsedComment, `@${serviceKey}`);
-    if (!tagText) {
+    const blockTagInfo = getBlockTagInfo(parsedComment, `@${serviceKey}`);
+    if (!blockTagInfo) {
       return;
     }
+    const { text } = blockTagInfo;
     try {
       // TODO: use something other than JSON to parse this
-      const value = JSON.parse(tagText);
+      const value = JSON.parse(text);
       result[serviceKey] = value;
     } catch (ex) {
       console.warn("Failed to parse ", serviceKey, "tag: ", ex);
@@ -59,14 +173,39 @@ export function extractServiceParameters(
   return result;
 }
 
-function getBlockTagContent(parsedComment: ParserContext, tagName: string) {
+function getBlockTagPosition(parsedComment: ParserContext, tagName: string) {
+  const block = getBlockTag(parsedComment, tagName);
+  if (!block) {
+    return null;
+  }
+
+  return {
+    start: block.blockTag.getTokenSequence().getContainingTextRange().pos,
+    end: block.blockTag.getTokenSequence().getContainingTextRange().end,
+  };
+}
+
+function getBlockTagInfo(parsedComment: ParserContext, tagName: string) {
+  const block = getBlockTag(parsedComment, tagName);
+  if (!block) {
+    return null;
+  }
+
+  return {
+    start: block.blockTag.getTokenSequence().getContainingTextRange().pos,
+    end: block.blockTag.getTokenSequence().getContainingTextRange().end,
+    text: extractPlainText(block.content).trim(),
+  };
+}
+
+function getBlockTag(parsedComment: ParserContext, tagName: string) {
   const block = parsedComment.docComment.customBlocks.find(
     ({ blockTag }) => blockTag.tagName === tagName
   );
   if (!block) {
     return null;
   }
-  return extractPlainText(block.content).trim();
+  return block;
 }
 
 function extractPlainText(d: DocNode): string {
