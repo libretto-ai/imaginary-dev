87d11c26d8ed792d09e6664b9e9a48aa1ebeff56
Alec Flett <alec@thegp.com>
2023-03-23 14:14:00 -0700

    use gpt-3.5-turbo (#21)

--- example-clients/nextjs-list-maker/src/imaginary.ts
+++ example-clients/nextjs-list-maker/src/imaginary.ts
@@ -1,6 +1,7 @@
 /**
  * This function takes a list of items, and comes up with a single name or
  * category that best describes the group as a whole.
+ * @openai `{"model": "gpt-3.5-turbo"}`
  *
  * @imaginary
  */
@@ -9,6 +10,7 @@ export declare function getNameForList(items: string[]): Promise<string>;
 /**
  * This function takes a category name, and a list of items in that category,
  * and returns one more items not in `items`, that would also a member of the named list.
+ * @openai `{"model": "gpt-3.5-turbo"}`
  *
  * @imaginary
  */
