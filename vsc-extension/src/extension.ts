977a5ef88299a3d310ead72a071b13ff1f15b063
Alec Flett <alecf@flett.org>
2023-03-24 17:03:10 -0700

    cleanup

--- vsc-extension/src/extension.ts
+++ vsc-extension/src/extension.ts
@@ -25,7 +25,6 @@ export function activate(context: vscode.ExtensionContext) {
   console.log = (message) => {
     ipChannel.appendLine(message);
   };
-  console.log("activation!");
 
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
@@ -33,73 +32,57 @@ export function activate(context: vscode.ExtensionContext) {
     'Congratulations, your extension "imaginary-programming" is now active!'
   );
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "imaginary-programming.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Hello World from Imaginary Programming!"
-      );
-    }
-  );
-
-  context.subscriptions.push(disposable);
-
   const sources: SourceFileMap = {};
 
   const functionTreeProvider = new ImaginaryFunctionProvider(sources);
-  const rtdp = vscode.window.registerTreeDataProvider(
-    "functions",
-    functionTreeProvider
+
+  context.subscriptions.push(
+    vscode.window.registerTreeDataProvider("functions", functionTreeProvider)
   );
-  context.subscriptions.push(rtdp);
 
   context.subscriptions.push(
     vscode.workspace.onDidChangeTextDocument((e) => {
-      updateFile(e.document);
+      updateFile(sources, e.document);
+      functionTreeProvider.refresh();
     })
   );
   context.subscriptions.push(
     vscode.workspace.onDidOpenTextDocument((document) => {
-      updateFile(document);
+      updateFile(sources, document);
+      functionTreeProvider.refresh();
     })
   );
-
-  function updateFile(document: vscode.TextDocument) {
-    console.log(
-      "updateFile",
-      document.fileName,
-      document.languageId,
-      document.uri.scheme
-    );
-    if (document.languageId !== "typescript" || document.uri.scheme === "git") {
-      return;
-    }
-
-    const { fileName } = document;
-    const relativeFileName = getRelativePathToProject(fileName);
-    const code = document.getText();
-
-    const sourceFile = ts.createSourceFile(
-      relativeFileName,
-      code,
-      // TODO: get this from tsconfig for the project
-      ts.ScriptTarget.Latest
-    );
-
-    const functions = findFunctions(sourceFile);
-    sources[relativeFileName] = {
-      functions,
-      sourceFile,
-    };
-    functionTreeProvider.refresh();
-  }
+  context.subscriptions.push(
+    vscode.workspace.onDidCloseTextDocument((document) => {
+      const relativeFilePath = getRelativePathToProject(document.fileName);
+      delete sources[relativeFilePath];
+      functionTreeProvider.refresh();
+    })
+  );
 }
 
+function updateFile(sources: SourceFileMap, document: vscode.TextDocument) {
+  if (document.languageId !== "typescript" || document.uri.scheme === "git") {
+    return;
+  }
+
+  const { fileName } = document;
+  const relativeFileName = getRelativePathToProject(fileName);
+  const code = document.getText();
+
+  const sourceFile = ts.createSourceFile(
+    relativeFileName,
+    code,
+    // TODO: get this from tsconfig for the project
+    ts.ScriptTarget.Latest
+  );
+
+  const functions = findFunctions(sourceFile);
+  sources[relativeFileName] = {
+    functions,
+    sourceFile,
+  };
+}
 // This method is called when your extension is deactivated
 export function deactivate() {}
 
