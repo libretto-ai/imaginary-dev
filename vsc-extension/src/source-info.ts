f397ba794b776b97c1db2e082a5b1b60a14e05ca
Alec Flett <alecf@flett.org>
2023-03-28 17:54:41 -0700

    make a shared dir, move shared interfaces there

--- vsc-extension/src/source-info.ts
+++
@@ -1,7 +0,0 @@
-import * as ts from "typescript";
-
-export interface SourceFileInfo {
-  sourceFile: ts.SourceFile;
-  functions: ts.FunctionDeclaration[];
-}
-export type SourceFileMap = Record<string, SourceFileInfo>;
