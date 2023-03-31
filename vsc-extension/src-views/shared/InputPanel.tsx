8c496807f53d60749563220ec2acba3045f8d046
Alec Flett <alecf@flett.org>
2023-03-31 16:42:02 -0700

    initial test case panel

--- vsc-extension/src-views/shared/InputPanel.tsx
+++ vsc-extension/src-views/shared/InputPanel.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { useExtensionState } from "./ExtensionState";
 
-type Props = {};
+export const InputPanel = () => {
+  const { selectedFunction, testCases } = useExtensionState();
 
-export const InputPanel = (props: Props) => {
-  return <div>InputPanel</div>;
+  if (!selectedFunction) {
+    return <p>No function selected</p>;
+  }
+  const functionTestCases = testCases[
+    selectedFunction.fileName
+  ]?.testCases.find(
+    ({ functionName }) => selectedFunction.functionName === functionName
+  );
+  return (
+    <div>
+      <p>Test cases for {selectedFunction.functionName}</p>
+      {!functionTestCases && (
+        <p>
+          <i>No test cases yet</i>
+        </p>
+      )}
+      {!!functionTestCases && (
+        <ul>
+          {functionTestCases.testCases.map((testCase, index) => (
+            <pre key={index}>{JSON.stringify(testCase.inputs)}</pre>
+          ))}
+          <li></li>
+        </ul>
+      )}
+      <pre></pre>
+    </div>
+  );
 };
