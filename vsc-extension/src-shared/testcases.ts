b2bd45aef115470e9adde6e450d439ddddf739b3
Alec Flett <alecf@flett.org>
2023-04-06 16:36:53 -0700

    testcase manipulation broken out of inputpanel

--- vsc-extension/src-shared/testcases.ts
+++ vsc-extension/src-shared/testcases.ts
@@ -67,3 +67,79 @@ export function findTestCases(
     ({ functionName }) => functionName === functionNameTarget
   );
 }
+
+import { produce } from "immer";
+
+const emptyTestCase: FunctionTestCase = {
+  inputs: {},
+  name: "New test case",
+  output: {
+    current: null,
+    prev: null,
+  },
+};
+
+function updateFunctionTestCase<T>(
+  functionTestCase: FunctionTestCase,
+  paramName: string,
+  value: T
+): FunctionTestCase {
+  return produce(functionTestCase, (draft) => {
+    draft.inputs[paramName] = value;
+  });
+}
+
+function updateFunctionTestCases<T>(
+  functionTestCases: FunctionTestCases,
+  index: number,
+  paramName: string,
+  value: T
+): FunctionTestCases {
+  return produce(functionTestCases, (draft) => {
+    draft.testCases = produce(draft.testCases, (draftTestCases) => {
+      draftTestCases[index] = updateFunctionTestCase(
+        draftTestCases[index] ?? emptyTestCase,
+        paramName,
+        value
+      );
+    });
+  });
+}
+export function updateSourcefileTestCase<T>(
+  sourceFileTestCases: SourceFileTestCaseMap,
+  sourceFileName: string,
+  functionName: string,
+  index: number,
+  paramName: string,
+  value: T
+): SourceFileTestCaseMap {
+  return produce(sourceFileTestCases, (draft) => {
+    draft[sourceFileName] = produce(
+      draft[sourceFileName] ?? { sourceFileName, functionTestCases: [] },
+      (draftTestCases) => {
+        const functionTestCaseIndex =
+          draftTestCases.functionTestCases.findIndex(
+            (testCase) => testCase.functionName === functionName
+          ) ?? { functionName, testCases: [] };
+        if (functionTestCaseIndex !== -1) {
+          draftTestCases.functionTestCases[functionTestCaseIndex] =
+            updateFunctionTestCases(
+              draftTestCases.functionTestCases[functionTestCaseIndex],
+              index,
+              paramName,
+              value
+            );
+        } else {
+          draftTestCases.functionTestCases.push(
+            updateFunctionTestCases(
+              { functionName, testCases: [] },
+              index,
+              paramName,
+              value
+            )
+          );
+        }
+      }
+    );
+  });
+}
--- vsc-extension/src/extension.ts
+++ vsc-extension/src/extension.ts
@@ -1,8 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { makeSerializable } from "../src-shared/serialize-source";
 import {
-  makeSerializable,
   MaybeSelectedFunction,
   SourceFileMap,
 } from "../src-shared/source-info";
