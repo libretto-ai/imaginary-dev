130b7542d5a2391d96991e1b538e7fcf1c254bac
Alec Flett <alec@thegp.com>
2023-04-10 09:45:24 -0700

    Add some tests for utility functions (#63)

---
+++ vsc-extension/src/util/serialized-source.test.ts
@@ -0,0 +1,93 @@
+import { MaybeSelectedFunction } from "../../src-shared/source-info";
+import { findMatchingFunction } from "./serialized-source";
+
+describe("findMatchingFunction", () => {
+  it("should return undefined if no selected function is provided", () => {
+    const sources = {
+      "test-file.ts": {
+        sourceFile: {
+          fileName: "test-file.ts",
+        },
+        functions: [],
+      },
+    };
+    const selectedFunction: MaybeSelectedFunction = null;
+
+    const result = findMatchingFunction(sources, selectedFunction);
+
+    expect(result).toBeUndefined();
+  });
+
+  it("should return undefined if no matching source file is found", () => {
+    const sources = {
+      "test-file.ts": {
+        sourceFile: {
+          fileName: "test-file.ts",
+        },
+        functions: [],
+      },
+    };
+    const selectedFunction: MaybeSelectedFunction = {
+      fileName: "other-file.ts",
+      functionName: "myFunction",
+    };
+
+    const result = findMatchingFunction(sources, selectedFunction);
+
+    expect(result).toBeUndefined();
+  });
+
+  it("should return undefined if no matching function is found", () => {
+    const sources = {
+      "test-file.ts": {
+        sourceFile: {
+          fileName: "test-file.ts",
+        },
+        functions: [
+          {
+            name: "otherFunction",
+            declaration: "",
+            parameters: [],
+          },
+        ],
+      },
+    };
+    const selectedFunction: MaybeSelectedFunction = {
+      fileName: "test-file.ts",
+      functionName: "myFunction",
+    };
+
+    const result = findMatchingFunction(sources, selectedFunction);
+
+    expect(result).toBeUndefined();
+  });
+
+  it("should return the matching function", () => {
+    const sources = {
+      "test-file.ts": {
+        sourceFile: {
+          fileName: "test-file.ts",
+        },
+        functions: [
+          {
+            name: "myFunction",
+            declaration: "",
+            parameters: [],
+          },
+        ],
+      },
+    };
+    const selectedFunction: MaybeSelectedFunction = {
+      fileName: "test-file.ts",
+      functionName: "myFunction",
+    };
+
+    const result = findMatchingFunction(sources, selectedFunction);
+
+    expect(result).toEqual({
+      name: "myFunction",
+      declaration: "",
+      parameters: [],
+    });
+  });
+});
