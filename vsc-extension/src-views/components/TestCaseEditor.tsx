1f19e382b825873c0842e777601ad5c45396fff8
Sasha Aickin <sasha@aickin.com>
2023-04-12 22:16:02 -0400

    Temporarily removed temperature editor and previous output column. (#76)

--- vsc-extension/src-views/components/TestCaseEditor.tsx
+++ vsc-extension/src-views/components/TestCaseEditor.tsx
@@ -35,7 +35,7 @@ export const TestCaseEditor: FC<Props> = ({
           selectedTestCaseIndex={selectedTestCaseIndex}
         />
       ))}
-      <TemperatureEditor />
+      {/* <TemperatureEditor /> */}
     </div>
   );
 };
