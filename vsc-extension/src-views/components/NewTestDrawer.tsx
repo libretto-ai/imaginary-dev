3dc3203ac2f9a2905d79a92113616f0d8a492b8a
Alec Flett <alecf@flett.org>
2023-04-20 16:05:09 -0700

    fully create the test and move on

--- vsc-extension/src-views/components/NewTestDrawer.tsx
+++ vsc-extension/src-views/components/NewTestDrawer.tsx
@@ -1,12 +1,20 @@
 import { VSCodeButton } from "@vscode/webview-ui-toolkit/react";
 import React, { FC, useCallback, useEffect, useState } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import {
   FunctionTestCase,
   SerializableFunctionDeclaration,
 } from "../../src-shared/source-info";
-import { addFunctionTestCase, blankTestCase } from "../../src-shared/testcases";
-import { selectedFunctionState, testCasesState } from "../shared/state";
+import {
+  addFunctionTestCase,
+  blankTestCase,
+  findTestCases,
+} from "../../src-shared/testcases";
+import {
+  selectedFunctionState,
+  selectedTestCaseIndexState,
+  testCasesState,
+} from "../shared/state";
 import { Drawer } from "./Drawer";
 import { TestCaseInputEditor } from "./TestCaseInputEditor";
 
@@ -23,6 +31,9 @@ export const NewTestDrawer: FC<Props> = ({
 }) => {
   const [testCases, setTestCases] = useRecoilState(testCasesState);
   const selectedFunction = useRecoilValue(selectedFunctionState);
+  const setTestCaseIndex = useSetRecoilState(
+    selectedTestCaseIndexState(selectedFunction)
+  );
   const [draftTestCase, setDraftTestCase] =
     useState<FunctionTestCase>(blankTestCase);
   const onUpdateDraftTestCase = (paramName: string, value: string): void =>
@@ -43,9 +54,28 @@ export const NewTestDrawer: FC<Props> = ({
       draftTestCase
     );
     setTestCases(newTestCases);
+
+    // Find the index of the new test (at the end) and select it
+    const totalTestCases =
+      findTestCases(
+        newTestCases,
+        selectedFunction.fileName,
+        selectedFunction.functionName
+      )?.testCases.length ?? 0;
+    setTestCaseIndex(totalTestCases - 1);
+
     onCloseDrawer();
+
+    // clear out state for the next one
     setDraftTestCase(blankTestCase);
-  }, [draftTestCase, onCloseDrawer, selectedFunction, setTestCases, testCases]);
+  }, [
+    draftTestCase,
+    onCloseDrawer,
+    selectedFunction,
+    setTestCaseIndex,
+    setTestCases,
+    testCases,
+  ]);
 
   // Close the drawer whenever the selected function changes
   useEffect(() => {
