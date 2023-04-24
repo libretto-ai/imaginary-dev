237799c7741e5263a9a206876301b8aead6c6d6b
Alec Flett <alecf@flett.org>
2023-04-24 09:50:37 -0700

    do not need to implement position

--- vsc-extension/__mocks__/vscode.ts
+++ vsc-extension/__mocks__/vscode.ts
@@ -1,10 +1,9 @@
 import * as mock from "jest-mock-vscode";
-import vscode from "vscode";
 
 const { URI: Uri, Utils } = jest.requireActual("vscode-uri");
 Uri.joinPath = Utils.joinPath;
 
-const m = {
+const localMock = {
   Uri,
   workspace: {
     workspaceFolders: [
@@ -17,40 +16,9 @@ const m = {
   window: {
     showTextDocument: jest.fn(),
   },
-  Position: jest.fn().mockImplementation(function (): vscode.Position {
-    return {
-      line: 0,
-      character: 0,
-      isBefore(other) {
-        return false;
-      },
-      compareTo(other) {
-        return 0;
-      },
-      isAfter(other) {
-        return false;
-      },
-      isAfterOrEqual(other) {
-        return false;
-      },
-      isBeforeOrEqual(other) {
-        return false;
-      },
-      isEqual(other) {
-        return false;
-      },
-      translate() {
-        return this;
-      },
-      with() {
-        return this;
-      },
-    };
-  }),
-  Range: jest.fn(),
 };
 
 export default {
   ...mock,
-  ...m,
+  ...localMock,
 };
